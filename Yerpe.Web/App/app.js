ff171313e0ed11dea017865f41980d14f93c149b
Graham McVea <graham.mcvea@hotmail.co.uk>
2015-03-22 10:31:54 +0000

    Add client side encryption

--- Yerpe.Web/App/app.js
+++ Yerpe.Web/App/app.js
@@ -1,6 +1,6 @@
-﻿var yerpeModule = angular.module("yerpeModule", ['ngRoute', 'ui.bootstrap', 'angular-loading-bar', 'SignalR']);
+﻿var yerpeModule = angular.module("yerpeModule", ['ngRoute', 'ui.bootstrap', 'angular-loading-bar', 'SignalR', 'mdo-angular-cryptography']);
 
-yerpeModule.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+yerpeModule.config(["$routeProvider", "$locationProvider", "$cryptoProvider", function ($routeProvider, $locationProvider, $cryptoProvider) {
 
     $routeProvider.when("/", {
         controller: "yerpeController",
@@ -24,9 +24,11 @@ yerpeModule.config(["$routeProvider", "$locationProvider", function ($routeProvi
         requireBase: false
     });
 
+    $cryptoProvider.setCryptographyKey('M1QnJM/XkqPpw5JF7DKhpQkyYgw2VfSpcGqHfRA5cs0=');
+
 }]);
 
-yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
+yerpeModule.factory("dataService", ["$http", "$q", '$crypto', function ($http, $q, $crypto) {
 
     var _messages = [];
     var _rooms = [];
@@ -41,7 +43,7 @@ yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
     var _roomId = 0;
     var _message = "";
 
-    var _isReady = function() {
+    var _isReady = function () {
         return _isInit;
     };
 
@@ -70,6 +72,11 @@ yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
               // success
               angular.copy(result.data.UsersInRoom, _usersInRoom);
               _messages.push.apply(_messages, result.data.Messages);
+
+              _messages.forEach(function (obj, i) {
+                  obj.Message = $crypto.decrypt(obj.Message);
+              });
+
               _myName = result.data.Name;
               _isAdmin = result.data.IsAdmin;
               _skip += 10;
@@ -92,6 +99,10 @@ yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
         $http.get("/api/yerpe/messages/" + _skip)
           .then(function (result) {
               // success
+              result.data.Messages.forEach(function (obj, i) {
+                  obj.Message = $crypto.decrypt(obj.Message);
+              });
+
               _messages.unshift.apply(_messages, result.data.Messages);
               _skip += 10;
               deferred.resolve();
@@ -179,7 +190,7 @@ yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
         usersInRoom: _usersInRoom,
         getMessages: _getMessages,
         getMoreMessages: _getMoreMessages,
-        isReady: _isReady, 
+        isReady: _isReady,
         unRead: _unRead,
         myName: _myName,
         getName: _getName,
@@ -194,22 +205,23 @@ yerpeModule.factory("dataService", ["$http", "$q", function ($http, $q) {
     };
 }]);
 
-yerpeModule.factory('yerpeHub', ['$rootScope', 'Hub', 'dataService', '$q', function ($rootScope, Hub, dataService, $q) {
+yerpeModule.factory('yerpeHub', ['$rootScope', 'Hub', 'dataService', '$q', '$crypto', function ($rootScope, Hub, dataService, $q, $crypto) {
 
     //declaring the hub connection
     var hub = new Hub('YerpeHub', {
 
         //client side methods
-        listeners:{
+        listeners: {
             'addNewMessageToPage': function (message) {
-                    dataService.receivedMessage = true;
-                    dataService.messages.push(message);
-                    var fullName = dataService.getName();
-                    if (message.From != fullName.substr(0, fullName.indexOf('@'))) {
-                        dataService.unRead += 1;
-                        $.titleAlert("(" + dataService.unRead + ")");
-                    }
-                    $rootScope.$apply();
+                message.Message = $crypto.decrypt(message.Message);
+                dataService.receivedMessage = true;
+                dataService.messages.push(message);
+                var fullName = dataService.getName();
+                if (message.From != fullName.substr(0, fullName.indexOf('@'))) {
+                    dataService.unRead += 1;
+                    $.titleAlert("(" + dataService.unRead + ")");
+                }
+                $rootScope.$apply();
             }
         },
 
@@ -217,7 +229,7 @@ yerpeModule.factory('yerpeHub', ['$rootScope', 'Hub', 'dataService', '$q', funct
         methods: ['Send'],
 
         //handle connection error
-        errorHandler: function(error){
+        errorHandler: function (error) {
             console.error(error);
         },
 
@@ -225,7 +237,8 @@ yerpeModule.factory('yerpeHub', ['$rootScope', 'Hub', 'dataService', '$q', funct
 
     var send = function (name, message) {
         var deferred = $q.defer();
-        deferred.resolve( hub.Send(name, message));
+        var encryptedMessage = $crypto.encrypt(message);
+        deferred.resolve(hub.Send(name, encryptedMessage));
         return deferred.promise;
     };
 
@@ -234,13 +247,13 @@ yerpeModule.factory('yerpeHub', ['$rootScope', 'Hub', 'dataService', '$q', funct
     };
 }]);
 
-yerpeModule.directive('globalEvents', function (dataService) {
-    return function (scope, element, attrs) {
-        element.bind('click', function (e) {
+yerpeModule.directive('globalEvents', function(dataService) {
+    return function(scope, element, attrs) {
+        element.bind('click', function(e) {
             dataService.unRead = 0;
-        })
-    }
-})
+        });
+    };
+});
 
 yerpeModule.directive('autoFocus', function ($timeout) {
     return {
@@ -253,100 +266,102 @@ yerpeModule.directive('autoFocus', function ($timeout) {
     };
 });
 
-yerpeModule.controller("yerpeController", function ($scope, $http, dataService, yerpeHub, $location, $anchorScroll, $window) {
+yerpeModule.controller("yerpeController", function($scope, $http, dataService, yerpeHub, $location, $anchorScroll, $window) {
     $scope.data = dataService;
 
     if (!dataService.isReady()) {
         dataService.getMessages()
-          .then(function () {
-              scrollToMessage();
-          },
-          function () {
-              alert("could not load messages");
-        });
+            .then(function() {
+                scrollToMessage();
+            },
+                function() {
+                    alert("could not load messages");
+                });
     }
 
-    $scope.send = function () {
-        yerpeHub.sendMessage(dataService.getName(), $scope.data.message)
-            .then(function () {
-                $scope.data.message = "";
-                scrollToMessage();
-            });    
+    $scope.send = function() {
+        if ($scope.data.message) {
+            yerpeHub.sendMessage(dataService.getName(), $scope.data.message)
+                .then(function() {
+                    $scope.data.message = "";
+                    scrollToMessage();
+                });
+        }
     };
 
-    $scope.getMoreMessages = function () {
+    $scope.getMoreMessages = function() {
         dataService.getMoreMessages()
-          .then(function (name) {
-              //success
-              scrollToTop();
-          },
-          function () {
-              //error
-              alert("could not load messages");
-          });
+            .then(function(name) {
+                //success
+                scrollToTop();
+            },
+                function() {
+                    //error
+                    alert("could not load messages");
+                });
     };
 
-    $scope.logOff = function () {
+    $scope.logOff = function() {
         $http.post("/account/logoff")
-            .then(function () {
+            .then(function() {
                 $window.location.reload();
             });
     };
 
-    $scope.$watchCollection('data.messages', function (newMessages, oldMessages) {
+    $scope.$watchCollection('data.messages', function(newMessages, oldMessages) {
         if (newMessages !== oldMessages && dataService.receivedMessage) {
             scrollToMessage();
             dataService.receivedMessage = false;
         }
     });
 
-    var scrollToMessage = function () {
+    var scrollToMessage = function() {
         $location.hash('message-' + (dataService.messages.length - 1));
         $anchorScroll();
     };
 
-    var scrollToTop = function () {
+    var scrollToTop = function() {
         $location.hash('message-0');
         $anchorScroll.yOffset = -50;
         $anchorScroll();
     };
 
-})
+});
 
-yerpeModule.controller("headController", function ($scope, dataService, $window) {
+yerpeModule.controller("headController", function($scope, dataService, $window) {
     $scope.data = dataService;
 
-    $window.onfocus = function () {
+    $window.onfocus = function() {
         dataService.unRead = 0;
     }
 
-})
+});
 
-yerpeModule.controller("roomController", function ($scope, dataService) {
+yerpeModule.controller("roomController", function($scope, dataService) {
     $scope.data = dataService;
     $scope.room = {};
 
-    $scope.init = function () {
+    $scope.init = function() {
         dataService.getRooms();
     };
 
-    $scope.createRoom = function () {
+    $scope.createRoom = function() {
         dataService.createRoom($scope.room);
     };
-    
-})
 
-yerpeModule.controller("userController", function ($scope, dataService) {
+});
+
+yerpeModule.controller("userController", function($scope, dataService) {
     $scope.data = dataService;
     $scope.user = {};
 
-    $scope.init = function () {
+    $scope.init = function() {
         dataService.getRooms();
         dataService.getUsers();
     };
 
-    $scope.createUser = function () {
+    $scope.createUser = function() {
         dataService.createUser($scope.user);
     };
 
-})
+});
---
+++ Yerpe.Web/Scripts/mdo-angular-cryptography.js
@@ -0,0 +1,34 @@
+﻿angular.module('mdo-angular-cryptography', [])
+    .provider('$crypto', function CryptoKeyProvider() {
+        var cryptoKey;
+
+        this.setCryptographyKey = function (value) {
+            this.cryptoKey = value;
+        }
+
+        this.$get = [function () {
+            return {
+                cryptoKey: this.cryptoKey,
+
+                encrypt: function (message, key) {
+
+                    if (key === undefined) {
+                        key = this.cryptoKey;
+                    }
+
+                    var encryptedMessage = CryptoJS.AES.encrypt(message, key);
+                    var iv = encryptedMessage.iv.toString();
+                    return encryptedMessage.toString();
+                },
+
+                decrypt: function (message, key) {
+
+                    if (key === undefined) {
+                        key = this.cryptoKey;
+                    }
+
+                    return CryptoJS.AES.decrypt(message, key).toString(CryptoJS.enc.Utf8)
+                }
+            }
+        }];
+    });
